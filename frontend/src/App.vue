a96f68e85b92f49b98a7e1a738c629d8f339f7da
Karl Hayek <k.hayek@groupeonepont.com>
2022-02-22 02:44:32 +0100

    Upgrade Vue to version 3

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,29 +1,33 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+// import HelloWorld from './components/HelloWorld.vue'
+import NavBar from './components/NavBar.vue'
+
+let count = ref(0);
+const isButtonDisabled = false;
+
+
+function increment() {
+  count.value++;
+}
+</script>
+
 <template>
-  <div id="app">
+  <header>
     <NavBar></NavBar>
 
+    <!-- <div class="wrapper"> -->
+    <!-- <HelloWorld msg="You did it!" />
+    </div>-->
+  </header>
+
+  <main>
     <div class="container">
+      <button :disabled="isButtonDisabled" v-on:click="increment">{{ count }}</button>
       Enter the name of the page you want to retrieve:
     </div>
-  </div>
+  </main>
 </template>
-
-<script>
-import NavBar from "./components/NavBar.vue";
-
-export default {
-  name: "App",
-  components: {
-    NavBar,
-  },
-
-  data() {
-    return {};
-  },
-
-  methods: {},
-};
-</script>
 
 <style>
 #app {
@@ -33,4 +37,3 @@ export default {
   color: #2c3e50;
 }
 </style>
-  
--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -1,34 +1,23 @@
 <template>
-  <body>
-    <nav class="navbar navbar-expand-md navbar-dark bg-dark mb-4">
-      <div class="container">
-        <a class="navbar-brand" href>{{ appName }}</a>
+<body>
+  <nav class="navbar navbar-expand-md navbar-dark bg-dark mb-4">
+    <div class="container">
+      <a class="navbar-brand">{{ appName }}</a>
 
-        <ul class="navbar-nav">
-          <li class="nav-item active">
-            <!-- <a href="https://www.groupeonepoint.com/" target="_blank">
-            <img src="@/assets/logo_onepoint.jpg" />
-          </a>-->
-            <!-- <img src="@/assets/logo_onepointbdf.png" /> -->
-            <!-- <img src="@/assets/logo_onepoint.jpg" /> -->
-          </li>
-        </ul>
-      </div>
-    </nav>
-  </body>
+      <ul class="navbar-nav">
+        <li class="nav-item active"></li>
+      </ul>
+    </div>
+  </nav>
+</body>
 </template>
 
 
-<script>
-export default {
-  name: "NavBar",
-  data() {
-    return {
-      appName: process.env.VUE_APP_NAME,
-    };
-  },
-};
+<script setup lang="ts">
+let appName = import.meta.env.VITE_APP_NAME;
+
 </script>
+
 
 <style>
 .nav-item > img {
