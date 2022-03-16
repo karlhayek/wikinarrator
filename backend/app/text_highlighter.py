2b24fa10f83ae942b0a705a5d2287f07c09464b2
Karl Hayek <k.hayek@groupeonepont.com>
2022-03-16 20:07:53 +0100

    Refactor text highlighting

--- backend/app/text_highlighter.py
+++ backend/app/text_highlighter.py
@@ -11,72 +11,58 @@ if not FRENCH_DICT.exists():    # Check in parent directory
 FRENCH_WORDS = set(FRENCH_DICT.read_text().splitlines())
 
 
-def highlight_non_french_words(text: str, html_tag: str):
-    html_tag_start, html_tag_end = f"<{html_tag}>", f"</{html_tag}>"
-    offset = 0
-    offset_increment = len(html_tag_start) + len(html_tag_end)
-    french_words_regex = r"[A-Za-zÀ-ÖØ-öø-ÿœ]{3,}"  # Words of at least 3 letters that can have accents
+def highlight(text: str, html_tag: str, reg_expr: str, should_highlight= (lambda _: True)) -> str:
+    """ Adds html tags to text based on the given regular expression and should_highlight function
+    Args:
+        text (str): Input text
+        html_tag (str): Tag to add around the desired text to highlight
+        reg_expr (str): Regular 
+        should_highlight (function, optional): Function deciding whether to highlight the match
+                                            resulting from the regex. Defaults to (lambda _: True).
+    Returns:
+        str: Text with the parts we wish to highlight inside html tags
+    """
 
-    for match in re.finditer(french_words_regex, text):
-        start_pos, end_pos = match.start() + offset, match.end() + offset
-        word = match.group()
-
-        # Check if lowercased and ascii'd word is in the dictionary (e.g. 'Sœur' to 'soeur')
-        if word.lower() not in FRENCH_WORDS and unidecode.unidecode(word.lower()) not in FRENCH_WORDS:
-            # Remove noms propres, but keep acronyms ?
-            if (not word[0].isupper() or word.isupper()):
-                text = text[:start_pos] + html_tag_start + text[start_pos: end_pos] +  html_tag_end + text[end_pos:]
-                offset += offset_increment
-
-    return text
-
-
-def highlight_long_sentences(text: str, html_tag: str, len_treshold: int = 225):
     html_tag_start, html_tag_end = f"<{html_tag}>", f"</{html_tag}>"
     offset = 0
     offset_increment = len(html_tag_start) + len(html_tag_end)
 
-    for match in re.finditer(r".+?[.!?\n]", text):
+    for match in re.finditer(reg_expr, text):
         start_pos, end_pos = match.start() + offset, match.end() + offset
-        
-        # If sentence has a length above 225 characters, highlight it
-        if end_pos - start_pos > len_treshold:
+
+        if should_highlight(match.group()):
             text = text[:start_pos] + html_tag_start + text[start_pos: end_pos] +  html_tag_end + text[end_pos:]
             offset += offset_increment
-
-    return text
-
-def highlight_text_in_quotations(text: str, html_tag: str):
-    html_tag_start, html_tag_end = f"<{html_tag}>", f"</{html_tag}>"
-    offset = 0
-    offset_increment = len(html_tag_start) + len(html_tag_end)
-
-    for match in re.finditer(r"«.+?»", text):
-        start_pos, end_pos = match.start() + offset, match.end() + offset
-
-        text = text[:start_pos] + html_tag_start + text[start_pos: end_pos] +  html_tag_end + text[end_pos:]
-        offset += offset_increment
-
-    return text
-
-def highlight_text_in_lists(text: str, html_tag: str):
-    html_tag_start, html_tag_end = f"<{html_tag}>", f"</{html_tag}>"
-    offset = 0
-    offset_increment = len(html_tag_start) + len(html_tag_end)
-
-    for match in re.finditer(r"(?<=\n)- .+?\n", text):
-        start_pos, end_pos = match.start() + offset, match.end() + offset
-
-        text = text[:start_pos] + html_tag_start + text[start_pos: end_pos] +  html_tag_end + text[end_pos:]
-        offset += offset_increment
-
-    return text
     
+    return text
+
+
+def __is_not_french_word(word: str) -> bool:
+    """ Cheks if given word is not in the French dictionary """
+    # Check if lowercased and ascii'd word is in the dictionary (e.g. 'Sœur' to 'soeur')
+    if word.lower() not in FRENCH_WORDS and unidecode.unidecode(word.lower()) not in FRENCH_WORDS:
+        # Remove noms propres, but keep acronyms ?
+        if (not word[0].isupper() or word.isupper()):
+            return True
+    return False
+
+
+def __is_long_sentence(sentence: str, len_treshold: int = 250) -> bool:
+    """ Checks if sentence length is above a given threshold """
+    return len(sentence) > len_treshold
+
+
 
 def highlight_text(text: str) -> str:
-    text = highlight_non_french_words(text, html_tag='hl_notfrench')
-    text = highlight_long_sentences(text, html_tag='hl_long', len_treshold=225)
-    text = highlight_text_in_quotations(text, html_tag='hl_quote')
-    text = highlight_text_in_lists(text, html_tag='hl_list')
+    """ Adds html tags to text for: non-french words; long sentences; sentences inside quotation marks, and lists """
+
+    # Highlight non-french words
+    text = highlight(text, "hl_notfrench", r"[A-Za-zÀ-ÖØ-öø-ÿœ]{3,}", should_highlight=__is_not_french_word)
+    # Highlight long sentences
+    text = highlight(text, "hl_long", r".+?[.!?\n]", should_highlight=__is_long_sentence)
+    # Highlight text inside quotation marks
+    text = highlight(text, "hl_quote", r"«.+?»")
+    # Highlight text inside lists
+    text = highlight(text, "hl_list", r"(?<=\n)- .+?\n")
 
     return text
